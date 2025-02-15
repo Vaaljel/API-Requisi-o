7025d8a64f3a6eabcdca483e5fefc4ac9a21a7a8
Vaaljel <vaaljoel108@gmail.com>
2025-02-15 06:16:10 +0000

    Ligar a Base de dados

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,9 @@
 package med.voll.api.controller;
 
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping; //Indica que um metodo dentro do controlador sera chamado quando uma requisição HTTPB POST for feita
 import org.springframework.web.bind.annotation.RequestBody; // Informa ao Spring que o corpo da requisição HTTP(jSON, XML) deve ser convertido e passado como argumento no metodo
 import org.springframework.web.bind.annotation.RequestMapping; //Define a o URL base do controlador
@@ -12,12 +14,13 @@ import org.springframework.web.bind.annotation.RestController; // Indica que a c
 
 public class MedicoController {
 
+    @Autowired
     private MedicoRepository repository;
 
     @PostMapping
     public void cadastraMedico(@RequestBody DadosCadastroMedico dados) {   /*O parâmetro @RequestBody DadosCadastroMedico dados indica que
                                                                                o corpo da requisição será passado
                                                                                como uma classe, para termos os dados detalhados de cada medico*/
-
+        repository.save(new Medico(dados));
     }
 }
