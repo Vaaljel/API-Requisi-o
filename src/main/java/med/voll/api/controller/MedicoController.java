3565f829a090bf9fbad12cfc9bcecb29c22571c2
Vaaljel <vaaljoel108@gmail.com>
2025-02-15 02:03:40 +0000

    entidade JPA

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,23 @@
+package med.voll.api.controller;
+
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.web.bind.annotation.PostMapping; //Indica que um metodo dentro do controlador sera chamado quando uma requisição HTTPB POST for feita
+import org.springframework.web.bind.annotation.RequestBody; // Informa ao Spring que o corpo da requisição HTTP(jSON, XML) deve ser convertido e passado como argumento no metodo
+import org.springframework.web.bind.annotation.RequestMapping; //Define a o URL base do controlador
+import org.springframework.web.bind.annotation.RestController; // Indica que a classe é responsavel por lidar com requisições HTTP e retorna respostas (jSON, XML)
+
+@RestController //Indica classe controller
+@RequestMapping("medicos") //Indica a URL base
+
+public class MedicoController {
+
+    private MedicoRepository repository;
+
+    @PostMapping
+    public void cadastraMedico(@RequestBody DadosCadastroMedico dados) {   /*O parâmetro @RequestBody DadosCadastroMedico dados indica que
+                                                                               o corpo da requisição será passado
+                                                                               como uma classe, para termos os dados detalhados de cada medico*/
+
+    }
+}
